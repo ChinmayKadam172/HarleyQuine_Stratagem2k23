3351f30fe76f2d4009bc1f933a9f34af5ab03b8c
LAPTOP-0BE72HIK\afifk <afifkhanab@gmail.com>
2023-03-17 23:22:49 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import FaceRec from "./components/FaceRec";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
 import HomeScreen from "./components/Landing Page/HomeScreen";
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+
+
 
 function App() {
   return (
     <div className="screen">
+    <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/face" element={<FaceRec />} />
+          <Route exact path='/' element={<ProtectedRoute/>}>
+            <Route exact path='/face' element={<FaceRec/>}/>
+          </Route>
+            <Route exact path='/' element={<HomeScreen/>}/>
+            <Route exact path='/login' element={<Login/>}/>
+            <Route exact path='/signup' element={<Signup/>}/>
         </Routes>
       </BrowserRouter>
+    </AuthProvider>
     </div>
   );
 }
