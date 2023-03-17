bc33197073e857f542537e60129d8286acc8e5ec
punIN10ded <imnotfreenow@gmail.com>
2023-03-18 00:30:36 +0530

    landing page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import FaceRec from "./components/FaceRec";
 import HomeScreen from "./components/Landing Page/HomeScreen";
 
 function App() {
-  return (
-    <div className="screen">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/face" element={<FaceRec />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className="screen">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<HomeScreen />} />
+					<Route path="/face" element={<FaceRec />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
